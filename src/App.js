3353550d7217b62fd8082d9b05036ffd3487a178
darlingbryan <bryangumpic@gmail.com>
2019-12-03 06:44:16 -0500

    Intergrate sign up component.

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Header from "./components/header/Header";
 import HomePage from "./pages/homepage/HomePage";
 import ShopPage from "./pages/shop/ShopPage";
 import SignIn from "./pages/signin/SignIn";
-import { auth } from "./firebase/firebase.utils";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 
 class App extends React.Component {
   constructor(props) {
@@ -19,10 +19,23 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user });
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
 
-      console.log(user);
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          });
+
+          console.log(this.state);
+        });
+
+        this.setState({ currentUser: userAuth });
+      }
     });
   }
 
--- src/pages/signin/SignIn.js
+++ src/pages/signin/SignIn.js
@@ -1,9 +1,15 @@
 import React from "react";
 import "./SignIn.scss";
 import Signin from "../../components/siginin/Signin";
+import Signup from "../../components/signup/Signup";
 
 const SigninAndSignout = () => {
-  return <Signin />;
+  return (
+    <div className="sign-in-and-sign-up">
+      <Signin />
+      <Signup />
+    </div>
+  );
 };
 
 export default SigninAndSignout;
