856f9173f3e2c6e35fde0bd57ac5aa104d9c0610
darlingbryan <bryangumpic@gmail.com>
2019-12-06 23:33:07 -0500

    moved directory and shop data to redux.

--- src/pages/shop/ShopPage.js
+++ src/pages/shop/ShopPage.js
@@ -1,24 +1,14 @@
-import React, { Component } from "react";
-import SHOP_DATA from "./ShopData";
-import CollectionPreview from "../../components/collection-preview/CollectionPreview";
-
-export class ShopPage extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      collections: SHOP_DATA
-    };
-  }
-  render() {
-    const { collections } = this.state;
-    return (
-      <div className="shop-page">
-        {collections.map(({ id, ...otherCollectionProps }) => (
-          <CollectionPreview key={id} {...otherCollectionProps} />
-        ))}
-      </div>
-    );
-  }
-}
+import React from "react";
+import CollectionsOverview from "../../components/collections-overview/CollectionsOverview";
+import { Route } from "react-router-dom";
+import CollectionPage from "../collection/Collection";
+const ShopPage = ({ match }) => {
+  return (
+    <div className="shop-page">
+      <Route exact path={`${match.path}`} component={CollectionsOverview} />
+      <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
+    </div>
+  );
+};
 
 export default ShopPage;
